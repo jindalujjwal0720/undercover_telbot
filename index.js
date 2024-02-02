88c7e8667e284e949daa519628c2c86314f4c0a0
Ujjwal Jindal <jindalujjwal0720@gmail.com>
2024-02-02 15:14:34 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const { handler } = require("./controllers");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("*", (req, res) => {
+  console.log(req.query);
+  res.send("Hello Undercover TelBot!");
+});
+
+app.post("*", async (req, res) => {
+  res.send((await handler(req)) || "ok");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
+});
+
+/**
+ * telegram webhook
+ * https://api.telegram.org/bot<token>/setWebhook?url=<url>
+ */
